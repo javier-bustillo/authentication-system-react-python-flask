bccfbc67babec97dff54686d53215c6c28db3bb2
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-18 18:20:15 +0000

    front>component>form.js added

---
+++ src/front/js/component/form.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // console.log("email: " + email, "password: " + password);
+  return (
+    <form className="m-5">
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
