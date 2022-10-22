f338f02d6930ac64686170488187f133236d8013
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-22 18:52:45 +0000

    /signup added

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,11 +12,24 @@ import datetime
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+
+    if email == user:
+        return jsonify({"msg": "User already exists"}), 403
+    else:
+        password_hashed = current_app.bcrypt.generate_password_hash(
+            password).decode("utf-8")
+
+    user = User(email=email, password=password_hashed, is_active=True)
+    db.session.add(user)
+    db.session.commit()
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "msg": "User created"
     }
 
     return jsonify(response_body), 200
@@ -29,7 +42,7 @@ def login():
     user = User.query.filter_by(email=email).first()
 
     if user is None:
-        return jsonify({"msg": "User does not exist"}), 400
+        return jsonify({"msg": "User does not exist"}), 401
 
     bcrypt_password_check = current_app.bcrypt.check_password_hash(
         user.password, password)
@@ -37,5 +50,6 @@ def login():
     if email != user.email or bcrypt_password_check == False:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(minutes=10)))
-    return jsonify(access_token = access_token)
+    access_token = create_access_token(
+        identity=email, expires_delta=datetime.timedelta(minutes=10))
+    return jsonify(access_token=access_token)
