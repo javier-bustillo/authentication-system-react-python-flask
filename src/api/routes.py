20d5983cd96cd6fbe7419141974ed2a14297f991
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-22 17:47:31 +0000

    /logincheck user and status/bcrypt check password/token expiration

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,11 +1,13 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, current_app
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+
+import datetime
 
 api = Blueprint('api', __name__)
 
@@ -24,10 +26,16 @@ def handle_hello():
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    user = User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(email=email).first()
 
-    if email != user.email or password != user.password:
+    if user is None:
+        return jsonify({"msg": "User does not exist"}), 400
+
+    bcrypt_password_check = current_app.bcrypt.check_password_hash(
+        user.password, password)
+
+    if email != user.email or bcrypt_password_check == False:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
+    access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(minutes=10)))
+    return jsonify(access_token = access_token)
