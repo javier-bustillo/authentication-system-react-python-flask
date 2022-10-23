6ca0efd5a07ecc0a3427bff21624238208fb157c
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-23 09:03:38 +0000

    signup component created

---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,6 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import { Link, Navigate } from "react-router-dom";
+
+export const Signup = () => {};
